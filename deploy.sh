537e096cda9248c0bb2404bc72a25f0051959b50
yshir <yshir0020@gmail.com>
2020-12-12 12:32:04 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,28 +3,28 @@
 set -ex
 
 if [ ! $DEPLOY_ENV ]; then
-    echo 'Set deploy environment to environment variable $DEPLOY_ENV'
-    exit 1
+  echo 'Set deploy environment to environment variable $DEPLOY_ENV'
+  exit 1
 fi
 
 if [ ! $AWS_REGION ]; then
-    echo 'Set AWS default region to environment variable $AWS_REGION'
-    exit 1
+  echo 'Set AWS default region to environment variable $AWS_REGION'
+  exit 1
 fi
 
 if [ ! $AWS_ACCESS_KEY_ID ]; then
-    echo 'Set your AWS access key to environment variable $AWS_ACCESS_KEY_ID'
-    exit 1
+  echo 'Set your AWS access key to environment variable $AWS_ACCESS_KEY_ID'
+  exit 1
 fi
 
 if [ ! $AWS_SECRET_ACCESS_KEY ]; then
-    echo 'Set your AWS secret access key to environment variable $AWS_SECRET_ACCESS_KEY'
-    exit 1
+  echo 'Set your AWS secret access key to environment variable $AWS_SECRET_ACCESS_KEY'
+  exit 1
 fi
 
 if [ ! $AWS_ACCOUNT_ID ]; then
-    echo 'Set your AWS account id to environment variable $AWS_ACCOUNT_ID'
-    exit 1
+  echo 'Set your AWS account id to environment variable $AWS_ACCOUNT_ID'
+  exit 1
 fi
 
 echo 'OK'
